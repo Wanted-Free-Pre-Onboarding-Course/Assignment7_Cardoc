ce3c92988e9da87f3ed16507ca195e29abf5d1a9
epitone <epitoneplus@gmail.com>
2021-11-24 16:58:38 +0900

    [refac]: 불필요한 코드 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [UsersModule, ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {

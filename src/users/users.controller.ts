b05cd9fd00f74704f567937a897a3178ba586315
epitone <epitoneplus@gmail.com>
2021-11-24 15:42:42 +0900

    [feat]: 인터셉터 구성
    1. 성공적인 응답을 할 때, success, data 객체 전달

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { HttpExceptionFilter } from './../common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from './../common/interceptors/success.interceptor';
+import { UsersService } from './users.service';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+@UseInterceptors(SuccessInterceptor)
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get(':id')
+  getHello(@Param('id', ParseIntPipe) param: number): string {
+    console.log(param);
+    console.log(typeof param);
+    return this.userService.getHello();
+  }
+}

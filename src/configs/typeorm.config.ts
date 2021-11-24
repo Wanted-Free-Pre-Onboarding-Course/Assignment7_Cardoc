e639b37b21dc932c2bbaa21db697b5702648b3d3
epitone <epitoneplus@gmail.com>
2021-11-25 00:27:35 +0900

    [fix]: entity 설정 후 pgAdmin에 테이블 추가 되지 않던 문제 해결
    1. 경로 변경

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -13,7 +13,7 @@ export const typeOrmModuleOptions = {
     username: configService.get('DB_USERNAME'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_NAME'),
-    entities: [__dirname + '/..**/*.entity.{js,ts}'],
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true, //! set 'false' in production
     autoLoadEntities: true,
     logging: true,

5575206de5d95f83b3a904383aa5f60a06c2891e
epitone <epitoneplus@gmail.com>
2021-11-28 18:34:17 +0900

    [refac]: application 싱글톤 패턴 적용

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,51 @@
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new HttpExceptionFilter());
-  const PORT = process.env.PORT;
-  await app.listen(PORT);
+class Application {
+  private logger = new Logger(Application.name);
+  private PORT: string;
+  private DEV_MODE: boolean;
+
+  constructor(private server: NestExpressApplication) {
+    this.server = server;
+    this.PORT = process.env.PORT;
+    this.DEV_MODE = process.env.NODE_ENV === 'production' ? false : true;
+  }
+
+  private async setUpGlobalMiddleware() {
+    this.server.use(cookieParser());
+    this.server.useGlobalPipes(new ValidationPipe());
+    this.server.useGlobalFilters(new HttpExceptionFilter());
+  }
+
+  async bootstrap() {
+    await this.setUpGlobalMiddleware();
+    await this.server.listen(this.PORT);
+  }
+
+  startLog() {
+    if (this.DEV_MODE) {
+      this.logger.log(`✅ Server on http://localhost:${this.PORT}`);
+    } else {
+      this.logger.log(`✅ Server on port ${this.PORT}...`);
+    }
+  }
+
+  errorLog(error: string) {
+    this.logger.error(`🆘 Server error ${error}`);
+  }
 }
-bootstrap();
+async function bootstrap(): Promise<void> {
+  const server = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = new Application(server);
+  await app.bootstrap();
+  app.startLog();
+}
+
+bootstrap().catch((error) => {
+  new Logger('init').error(error);
+});
